f0b9b838bf0e671c1a16df3cee3e3ba845f9b490
xvax <jesse.vaisanen@cs.helsinki.fi>
2014-01-30 23:49:32 +0200

    luovutus

--- Asteroids/src/main/java/asteroids/asteroids/App.java
+++ Asteroids/src/main/java/asteroids/asteroids/App.java
@@ -8,12 +8,8 @@ import raw.VectorShape;
 public class App {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        
-        System.out.println(10/4);
-=======
+
         Physics p = new Physics();
         p.game();
->>>>>>> 4ac608764b3f1151ae990bea761f9a2b802344a3
     }
 }
